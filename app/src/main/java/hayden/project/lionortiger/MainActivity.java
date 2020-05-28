02880ecd968630c15b76541cf1778b2afea93b2a
Hayden Bazor <hbazor@murraystate.edu>
2020-05-27 23:42:48 -0500

    Added translate animation within onclick so that the image would move out of the grid.

--- app/src/main/java/hayden/project/lionortiger/MainActivity.java
+++ app/src/main/java/hayden/project/lionortiger/MainActivity.java
@@ -3,12 +3,37 @@ package hayden.project.lionortiger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img1,img2,img3,img4,img5,img6,img7,img8,img9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.img2);
+        img3 = findViewById(R.id.img3);
+        img4 = findViewById(R.id.img4);
+        img5 = findViewById(R.id.img5);
+        img6 = findViewById(R.id.img6);
+        img7 = findViewById(R.id.img7);
+        img8 = findViewById(R.id.img8);
+        img9 = findViewById(R.id.img9);
+
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                img1.animate().translationX(-400).setDuration(3000);
+            }
+        });
     }
+
+
+
 }
