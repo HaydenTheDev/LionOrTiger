dee328e62053bf24eacb0f8b4eca5be738ff01d6
Hayden Bazor <hbazor@murraystate.edu>
2020-05-28 09:13:22 -0500

    created tags for images, an array and 2D array 2 establish who wins the tic tac toe.

--- app/src/main/java/hayden/project/lionortiger/MainActivity.java
+++ app/src/main/java/hayden/project/lionortiger/MainActivity.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView whoTurn;
 
-        enum Player {
 
+        enum Player {
             one, two
         }
-
         Player currentPlayer = Player.one;
+
+        //array
+        Player[] playerChoice = new Player[9];
+
+        //2D array for winner.
+        int [][] arrayWinner = {{0,1,2},{3,4,5},{6,7,8}, {0,3,6},{1,4,7},{2,5,8}, {0,4,8},{2,4,6}};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +37,15 @@ public class MainActivity extends AppCompatActivity {
 
     //onClick method
     public void lionImageClicked(View imageView){
-
         ImageView tappedImageView = (ImageView) imageView;
         tappedImageView.setTranslationX(-2000);
 
+        int tiTag = Integer.parseInt(tappedImageView.getTag().toString());
+        playerChoice[tiTag] = currentPlayer;
         if(currentPlayer == Player.one) {
             tappedImageView.setImageResource(R.drawable.lion);
             whoTurn.setText(R.string.player2);
+
             currentPlayer = Player.two;
         }
         else if(currentPlayer == Player.two){
@@ -43,7 +53,11 @@ public class MainActivity extends AppCompatActivity {
             whoTurn.setText(R.string.player1);
             currentPlayer = Player.one;
         }
-        tappedImageView.animate().translationXBy(2000).alpha(1).rotation(720).setDuration(1000);
+        tappedImageView.animate().translationXBy(2000).
+                alpha(1).rotation(720).setDuration(1000);
+
+        Toast.makeText(this, tappedImageView.getTag().
+                toString(),Toast.LENGTH_SHORT).show();
 
 
 
